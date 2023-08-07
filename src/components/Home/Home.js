663fd117f113f0a4f1411d08db4956bdcf909602
atif <atifa901@gmail.com>
2023-08-07 12:10:17 +0530

    new changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,34 +3,34 @@ import "./Home.css"
 export const HomePage = () => (
     <div>
      
-     <div class="container-fluid position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
+     <div className="container-fluid position-sticky z-index-sticky top-0">
+    <div className="row">
+      <div className="col-12">
        
-        <nav class="navbar navbar-expand-lg blur  top-0 z-index-3 shadow position-absolute  py-2 start-0 end-0 ">
-          <div class="container-fluid pe-0">
-            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
+        <nav className="navbar navbar-expand-lg blur  top-0 z-index-3 shadow position-absolute  py-2 start-0 end-0 ">
+          <div className="container-fluid pe-0">
+            <a className="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="/">
               Soft UI Dashboard
             </a>
-            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon mt-2">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
+            <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon mt-2">
+                <span className="navbar-toggler-bar bar1"></span>
+                <span className="navbar-toggler-bar bar2"></span>
+                <span className="navbar-toggler-bar bar3"></span>
               </span>
             </button>
-            <div class="collapse navbar-collapse" id="navigation">
-              <ul class="navbar-nav mx-auto ms-xl-auto me-xl-7">
+            <div className="collapse navbar-collapse" id="navigation">
+              <ul className="navbar-nav mx-auto ms-xl-auto me-xl-7">
                
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="/signup">
-                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                <li className="nav-item">
+                  <a className="nav-link me-2" href="/signup">
+                    <i className="fas fa-user-circle opacity-6 text-dark me-1"></i>
                     Sign Up
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="/login">
-                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                <li className="nav-item">
+                  <a className="nav-link me-2" href="/login">
+                    <i className="fas fa-key opacity-6 text-dark me-1"></i>
                     Sign In
                   </a>
                 </li>
@@ -45,22 +45,22 @@ export const HomePage = () => (
     </div>
   </div>
 
-  <section class="min-vh-100 mb-8">
-    <div className="page-header align-items-center min-vh-100 pt-5 pb-11 " style={{backgroundImage: "url('/Img/curved-images/curved14.jpg')"}}>
-      <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-12 text-center mx-auto mt-5">
-            <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-            <p class=" h1 text-white mb-5">Grow your business globally with strategic partnerships</p>
+  <section className="min-vh-100 mb-8">
+    <div classNameName="page-header align-items-center min-vh-100 pt-5 pb-11 " style={{backgroundImage: "url('/Img/curved-images/curved14.jpg')"}}>
+      <span className="mask bg-gradient-dark opacity-6"></span>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-12 text-center mx-auto mt-5">
+            <h1 className="text-white mb-2 mt-5">Welcome!</h1>
+            <p className=" h1 text-white mb-5">Grow your business globally with strategic partnerships</p>
           </div>
         </div>
-        <div class="row justify-content-center">
-          <div class="col-lg-6 text-end ">
-            <a class="btn btn-round btn-sm mb-0 btn-outline-white " target="_blank" href="#">Join as an advertiser</a>
+        <div className="row justify-content-center">
+          <div className="col-lg-6 text-end ">
+            <a className="btn btn-round btn-sm mb-0 btn-outline-white " target="_blank" href="#">Join as an advertiser</a>
           </div>
-          <div class="col-lg-6 text-start ">
-            <a class="btn btn-round btn-sm mb-0 btn-outline-white " target="_blank" href="#">Join as a publisher</a>
+          <div className="col-lg-6 text-start ">
+            <a className="btn btn-round btn-sm mb-0 btn-outline-white " target="_blank" href="#">Join as a publisher</a>
           </div>
         </div>
       </div>
